904b40306c6fd1b1dd76e2fc750af4f15b8f0d01
aman1106 <amanpandey.lko@gmail.com>
2017-07-17 17:44:50 +0530

    comments added

--- wp_database_table.php
+++ wp_database_table.php
@@ -1,11 +1,18 @@
 <?php
 
+/**
+* function to create database tables when plugin is activated
+*/
 function wp_database_table() {
     global $wpdb;
     $table_name=$wpdb->prefix."wpds_display";
+    //condition to check whether table exits in database or not
     if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
     //table not in database. Create new table for displays
     $charset_collate = $wpdb->get_charset_collate();
+    /**
+    * database command to create table
+    */
     //creating table for wpds_displays
     $sql = "CREATE TABLE `wpds_displays` (
  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,15 +25,21 @@ function wp_database_table() {
  `status` enum('active','disabled') COLLATE utf8_unicode_ci DEFAULT 'active',
  PRIMARY KEY (`id`)
 ) $charset_collate;";
+    //including file upgrade.php
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    //modifies database
     dbDelta( $sql );
     }
 
 
     $table_name=$wpdb->prefix."wpds_events";
+    //condition to check whether table exits in database or not
     if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
       //table not in database. Create new table for events
       $charset_collate = $wpdb->get_charset_collate();
+      /**
+      * database command to create table
+      */
       //create table for wpds_events
       $sql = "CREATE TABLE `wpds_events` (
  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
@@ -39,15 +52,21 @@ function wp_database_table() {
  `status` enum('active','disabled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'active',
  PRIMARY KEY (`id`)
 ) $charset_collate;";
+     //including file upgrade.php
      require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+     //modifies database
      dbDelta( $sql );
     }
 
 
     $table_name=$wpdb->prefix."wpds_floormaps";
+    //condition to check whether table exits in database or not
     if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
       //table not in database. Create new table for floormaps
       $charset_collate = $wpdb->get_charset_collate();
+      /**
+      * database command to create table
+      */
       //create table for wpds_floormaps
       $sql = "CREATE TABLE `wpds_floormaps` (
  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -56,15 +75,21 @@ function wp_database_table() {
  `status` enum('active','disabled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'active',
  PRIMARY KEY (`id`)
 ) $charset_collate;";
+     //including file upgrade.php
      require_once(ABSPATH . 'wp-admin/includes/upgrade.php' );
+     //modifies database
      dbDelta( $sql );
    }
 
 
    $table_name=$wpdb->prefix."wpds_group_displays";
+   //condition to check whether table exits in database or not
    if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
      //table not in database. Create new table for group_displays
      $charset_collate = $wpdb->get_charset_collate();
+     /**
+     * database command to create table
+     */
      //create table for wpds_group_displays
      $sql = "CREATE TABLE `wpds_group_displays` (
  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
@@ -74,9 +99,12 @@ function wp_database_table() {
  `status` enum('active','disabled') COLLATE utf8_unicode_ci DEFAULT 'active',
  PRIMARY KEY (`id`)
 ) $charset_collate;";
+     //including file upgrade.php
      require_once(ABSPATH . 'wp-admin/includes/upgrade.php' );
+     //modifies database
      dbDelta( $sql );
    }
 }
+// registering plugin function
 register_activation_hook( __FILE__, 'wp_database_table' );
 ?>
